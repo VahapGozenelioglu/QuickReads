e530e32c3f2b9a6941e7f6f1b7ed7860e34a0b95
AbdullahKurt44 <abdullahkurt174@gmail.com>
2025-05-01 12:13:02 +0300

    Added GetAllArticlesByTag service

---
+++ QuickReads/Services/GetAllArticlesByTag.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using QuickReads.Contexts;
+using QuickReads.Entities.Enums;
+using QuickReads.Extensions;
+
+namespace QuickReads.Services;
+
+
+public class GetAllArticlesByTag
+{
+    private static ApplicationDbContext _context;
+
+    public class Request
+    {
+        public String TagName { get; set; }
+    }
+
+    public class Response
+    {
+        public List<ArticleDto> Articles { get; set; }
+    }
+
+    public GetAllArticlesByTag(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public Response Invoke(Request req)
+    {
+        var allArticles = _context.Articles
+            .Include(x => x.Tags)
+            .Select(x => x.ToDto())
+            .ToList();
+
+        var resp = allArticles.Select(x => x.Tags.Any( y => y.Name == req.TagName)).ToList();
+        
+        return new Response() { Articles = resp };
+    }
+}
